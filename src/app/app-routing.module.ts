77ee8ad751428f4b0ce2743cf670a704a094cf74
Ahmad Shabaneh <shabaneh.ahmad@gmail.com>
2019-11-05 09:29:08 -0500

    working on user request page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { NewRequestComponent } from './components/new-request/new-request.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+    path: 'new-request',
+    component: NewRequestComponent
+}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { LoginService } from './services/login.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
@@ -10,22 +10,27 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LoginComponent } from './components/login/login.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { RequestsComponent } from './components/requests/requests.component';
+import { NewRequestComponent } from './components/new-request/new-request.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         HeaderComponent,
         LoginComponent,
-        ProfileComponent
+        ProfileComponent,
+        RequestsComponent,
+        NewRequestComponent
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
         MDBBootstrapModule.forRoot(),
+        FormsModule,
         ReactiveFormsModule,
         HttpClientModule,
     ],
     providers: [LoginService],
-    bootstrap: [AppComponent, HeaderComponent, LoginComponent, ProfileComponent]
+    bootstrap: [AppComponent, HeaderComponent, LoginComponent, ProfileComponent, RequestsComponent, NewRequestComponent]
 })
 export class AppModule { }
