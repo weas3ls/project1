d0f0704f5ee6624b0348d149c8e9ad9d979a6a60
joeycentral@gmail.com <joeycentral@gmail.com>
2019-11-09 03:53:06 -0500

    Started working on ManagerServlet. Already implemented function to view all reimbursement tickets in addition to enforcing that the user is of correct role ID. Will implement a deny/approve function tomorrow.

---
+++ project1/src/main/java/com/revature/servlets/ManagerServlet.java
@@ -0,0 +1,51 @@
+package com.revature.servlets;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.revature.services.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.models.*;
+
+public class ManagerServlet extends HttpServlet {
+	private ReimbursementService reimbursementService = new ReimbursementService();
+	private UserServices userService = new UserServices();
+	
+	ObjectMapper om = new ObjectMapper();
+	
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession session = req.getSession(false);
+		if (session == null) {
+			resp.setStatus(400);
+			resp.getWriter().write("Session not found in manager tools.");
+			return;
+		}
+		int managerId = Integer.valueOf(session.getAttribute("userid").toString());
+		System.out.println("ManagerServlet managerId: " + managerId);
+		User isManager = userService.getUserById(managerId);
+		isManager.setPassword("");
+		isManager.setPasswordSalt("");
+		if (isManager != null) {
+			if (isManager.getRoleId() != 1) {
+				resp.setStatus(403);
+				resp.getWriter().write("Only manager has access to manager tools");
+				return;
+			}
+		} 
+		
+		List<Reimbursement> employeeList = reimbursementService.getAllTickets();
+		
+		om.writeValue(resp.getWriter(), employeeList);
+		resp.setStatus(200);
+	}
+}
